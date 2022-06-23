e82e9c5294f4ee9a4c662096be11b235ca5a8faf
Jenna Goldberg <Jenna.Goldberg@alaskaair.com>
2022-06-23 14:06:58 -0700

    added pagination and rewrote tag filtering

--- components/post.tsx
+++ components/post.tsx
@@ -14,36 +14,36 @@ export type PostProps={
 //this is a prop that arranges and styles the content of the post
 export default function Post(props:PostProps) {
     const ref = useRef<HTMLDivElement>(null);
-    const [postText, setPostText] = useState(props.post);
+    let paragraphs = props.post.split('\n');
+    const [postText, setPostText] = useState(paragraphs[0]);
     const [buttonText, setButtonText] = useState("Expand");
 
-    let altText = "this is alternative text";
 
     function switchPostSize() {
         if (postText === props.post) {
-            setPostText(altText);
-            setButtonText("Minimize");
+            setPostText(paragraphs[0]);
+            setButtonText("Expand");
         } else {
             setPostText(props.post);
-            setButtonText("Expand");
+            setButtonText("Minimize");
         }
     }
 
-    useEffect(() => {
-        if (!props.tags.some(tag => props.currentTag === tag) && props.currentTag !== null) {
-            ref.current ? ref.current.style.visibility='hidden' : null;
-        } else {
-            ref.current ? ref.current.style.visibility='visible' : null; 
-        }
-    }
-    )
+    // useEffect(() => {
+    //     if (!props.tags.some(tag => props.currentTag === tag) && props.currentTag !== null) {
+    //         ref.current ? ref.current.style.display='none' : null;
+    //     } else {
+    //         ref.current ? ref.current.style.display='block' : null; 
+    //     }
+    // }
+    // ) 
     
     return (
         <div ref={ref} className={styles.post}>
             <div className={styles.postDate}>{props.postDate}</div>
             <div className={styles.title}>{props.title}</div>
             <div>{postText}</div>
-            <button onClick={() => switchPostSize()}>{buttonText}</button>
+            <button className={styles.directoryTags} onClick={() => switchPostSize()}>{buttonText}</button>
             <div className={styles.tags}>{props.tags}</div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Post from '../components/post'
 import { PostProps } from '../components/post'
 import styles from '../styles/Home.module.css'
 import DirectoryTag from '../components/directoryTag'
-
+import { allPosts, PostData } from '../postData'
 
 
 const Home: NextPage = () => {
   const [currentTag, setTag] = useState<any>(null);
-  const allPosts:PostProps[] = [{postDate:"6/20/22", title:"first title", post: "lorem ipsum", tags:["test tag", "another tag"], currentTag}, 
-      {postDate:"6/21/22", title:"second title", post: "dolor dic amet", tags:["another tag"], currentTag}];
-  
-  const [buttonStyle, setButtonStyle] = useState<string[]>()
+  const [first, setFirst] = useState<any>(0);
+  const [filteredPosts, setFilteredPosts] = useState<PostData[]>(allPosts)
 
   let tagsArr:string[] = [];
   for (let i = 0; i < allPosts.length; i++) {
@@ -24,7 +22,28 @@ const Home: NextPage = () => {
   let uniqueTags = Array.from(uniqueTagsSet);
 
 
+  function updateFirst(direction:string) {
+    if ((direction == "right" && first+10 > filteredPosts.length) || (direction == "left" && first-10 < 0)) {
+      setFirst(first);
+    }
+    else if (direction == "right") {
+      setFirst(first+10);
+    } else {
+      setFirst(first-10);
+    }
+  }
 
+  useEffect(() => {
+    if (currentTag !== null) {
+      setFilteredPosts(allPosts.filter(singlePost => singlePost.tags.some(tag => tag === currentTag)));
+    } else {
+      setFilteredPosts(allPosts);
+    } 
+  }, [currentTag]);
+
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [first])
 
   return (
     <div className={styles.container}>
@@ -44,9 +63,7 @@ const Home: NextPage = () => {
               setTag={setTag}/> )}
         </div>
 
-        <div>{currentTag}</div>
-
-        {allPosts.map(singlePost => (
+        {filteredPosts.slice(first, first+10).map(singlePost => (
           <Post
             postDate={singlePost.postDate}   
             title={singlePost.title}
@@ -54,7 +71,11 @@ const Home: NextPage = () => {
             tags={singlePost.tags}
             currentTag={currentTag} />
         ))}
-
+        
+        <div className={styles.navButtonBox}>
+          <button className={styles.navButton} onClick={() => updateFirst("left")}>Back</button>
+          <button className={styles.navButton} onClick={() => updateFirst("right")}>Next</button>
+        </div>
 
 
       </main>
